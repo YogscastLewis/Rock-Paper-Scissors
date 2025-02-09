e28b7d1d13a6a7164360063752f89ccfcc82af4f
YogscastLewis <jakob.synott@gmail.com>
2025-02-09 22:51:05 +0100

    Finalise project with playRound and playGame func
    Added playRound and playGame functions which keep score, and play the game 5 times before ending the program

--- script.js
+++ script.js
@@ -2,65 +2,86 @@
 let computerChoice;
 let humanChoice;
 
+
 function getComputerChoice() {
 
     computerChoice = Math.floor( Math.random() * 3 );
-
+    
     if ( computerChoice == 0 ) {
-
         console.log("Computer: Rock");
-        
     } else if ( computerChoice == 1 ) {
-
         console.log("Computer: Paper");
-
     } else {
-
         console.log("Computer: Scissors");
-        
     }
+
+    return computerChoice
 }
 
 function getHumanChoice() {
 
-    humanChoice = prompt();
+    humanChoice = prompt().toUpperCase();
 
-    if ( humanChoice == "Rock" ) {
-
+    if ( humanChoice == "ROCK" ) {
         console.log("Human: Rock");
-        humanChoice = 0;
-
-    } else if ( humanChoice == "Paper" ) {
-
+        return 0;
+    } else if ( humanChoice == "PAPER" ) {
         console.log("Human: Paper");
-        humanChoice = 1;
-
-    } else if ( humanChoice == "Scissors" ) {
-
+        return 1;
+    } else if ( humanChoice == "SCISSORS" ) {
         console.log("Human: Scissors");
-        humanChoice = 2;
-
+        return 2;
     }
 }
 
-function gameLogic() {
-    if (humanChoice == 0) {
-        computerChoice == 0 ? console.log("Draw!") :
-        computerChoice == 1 ? console.log("You Lose!") :
-        console.log("You Win!")
-    } 
-    else if (humanChoice == 1) {
-        computerChoice == 0 ? console.log("You Win!") :
-        computerChoice == 1 ? console.log("Draw!") :
-        console.log("You Lose!")
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice == 0) {
+            if (computerChoice == 0) {
+                console.log("Draw!")
+            } else if (computerChoice == 1) {
+                console.log("You Lose!");
+                computerScore += 1; 
+            } else {
+                console.log("You Win!");
+                humanScore += 1;
+            }
+        } 
+        else if (humanChoice == 1) {
+            if (computerChoice == 0) {
+                console.log("You Win!");
+                humanScore += 1;
+            } else if (computerChoice == 1) {
+                console.log("Draw");
+            } else {
+                console.log("You Lose!");
+                computerScore += 1;
+            }
+        }
+        else {
+            if (computerChoice == 0) {
+                console.log("You Lose!");
+                computerScore += 1;
+            } else if (computerChoice == 1) {
+                console.log("You Win!");
+                humanScore += 1; 
+            } else {
+                console.log("Draw");
+            }
+        }
+        console.log("Human Score " + humanScore)
+        console.log("Computer Score " + computerScore)
+        console.log(" ")
     }
-    else {
-        computerChoice == 0 ? console.log("You Lose!") :
-        computerChoice == 1 ? console.log("You Win!") :
-        console.log("Draw!")
-    }
+
+    playRound(getHumanChoice(),getComputerChoice());
+    playRound(getHumanChoice(),getComputerChoice());
+    playRound(getHumanChoice(),getComputerChoice());
+    playRound(getHumanChoice(),getComputerChoice());
+    playRound(getHumanChoice(),getComputerChoice());
 }
 
-getHumanChoice();
-getComputerChoice();
-gameLogic();
+playGame();
